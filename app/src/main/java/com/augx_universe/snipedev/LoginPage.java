d98be13acafb8ae2626e8aa642e98a9937295b66
praveen kumar <praveen0533y@gmail.com>
2023-03-13 07:03:34 +0530

    started ui works

--- app/src/main/java/com/augx_universe/snipedev/LoginPage.java
+++ app/src/main/java/com/augx_universe/snipedev/LoginPage.java
@@ -1,14 +1,21 @@
 package com.augx_universe.snipedev;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
 
 public class LoginPage extends AppCompatActivity {
+    CardView email,password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+
+        email = findViewById(R.id.userName);
+        email.setBackgroundResource(R.drawable.card_backgorund);
+        password = findViewById(R.id.password);
+        password.setBackgroundResource(R.drawable.card_backgorund);
     }
 }
