d4ff325f387627cf7efe8c9c347fdeb5e3093cf0
praveen kumar <praveen0533y@gmail.com>
2023-03-16 09:12:27 +0530

    started ui works

---
+++ app/src/main/java/com/augx_universe/snipedev/SignUpFragment.java
@@ -0,0 +1,22 @@
+package com.augx_universe.snipedev;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class SignUpFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+
+        View view = inflater.inflate(R.layout.fragment_signup,container,false);
+        return view;
+    }
+}
