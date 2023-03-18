b24cf67a41506659e61f70fe2d188d6a3c08ea42
praveen kumar <praveen0533y@gmail.com>
2023-03-18 09:26:15 +0530

    started ui works

--- app/src/main/java/com/augx_universe/snipedev/LoginPage.java
+++ app/src/main/java/com/augx_universe/snipedev/LoginPage.java
@@ -1,21 +1,34 @@
 package com.augx_universe.snipedev;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 
-public class LoginPage extends AppCompatActivity {
-    CardView email,password;
+public class LoginPage extends Fragment {
+        CardView userName,password;
+     public  LoginPage(){
 
+     }
+
+
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_page);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        email = findViewById(R.id.userName);
-        email.setBackgroundResource(R.drawable.card_backgorund);
-        password = findViewById(R.id.password);
-        password.setBackgroundResource(R.drawable.card_backgorund);
+        View view  = inflater.inflate(R.layout.activity_login_page,container,false);
+
+
+        return view;
     }
 }
--- app/src/main/java/com/augx_universe/snipedev/SignUpFragment.java
+++ app/src/main/java/com/augx_universe/snipedev/SignUpFragment.java
@@ -11,6 +11,9 @@ import androidx.fragment.app.Fragment;
 
 public class SignUpFragment extends Fragment {
 
+    public SignUpFragment(){
+
+    }
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
