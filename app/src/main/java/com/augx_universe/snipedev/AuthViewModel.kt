fcef3b2685077cf7602a6110a21d5de5c53c03a9
praveen kumar <praveen0533y@gmail.com>
2023-04-21 11:10:36 +0530

    Tweaking UI to make interactive

--- app/src/main/java/com/augx_universe/snipedev/AuthViewModel.kt
+++ app/src/main/java/com/augx_universe/snipedev/AuthViewModel.kt
@@ -14,6 +14,7 @@ class AuthViewModel() : ViewModel()
     var password: String? = null
     var database: UserAuthByEmail = UserAuthByEmail()
 
+
   private var _isNewUserCreated = MutableLiveData<Boolean>()
   val isNewUserCreated: LiveData<Boolean> = _isNewUserCreated
 
--- app/src/main/java/com/augx_universe/snipedev/SignUpActivity.kt
+++ app/src/main/java/com/augx_universe/snipedev/SignUpActivity.kt
@@ -3,15 +3,28 @@ package com.augx_universe.snipedev
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.InputType
+import android.text.TextWatcher
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.augx_universe.snipedev.databinding.FragmentSignupBinding
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.EmailAuthCredential
 
 class SignUpActivity : AppCompatActivity() {
     lateinit var binding: FragmentSignupBinding
     lateinit var rViewModel: AuthViewModel
+    lateinit var weakTextView: TextView
+    private lateinit var passwordListener: TextView
+    lateinit var monkeyReaction: ImageView
+    var isMonkeyOpen: Boolean = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.fragment_signup)
@@ -31,5 +44,52 @@ class SignUpActivity : AppCompatActivity() {
 
         }
 
+        weakTextView = binding.signUpWeakTextPassword
+
+        passwordListener = binding.passwordInput
+        passwordListener.addTextChangedListener(object :TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s != null) {
+                    if (s.length <= 6){
+                    weakTextView.text = "Password length not less than 6 characters! "
+                        weakTextView.setTextColor(resources.getColor(R.color.red))
+                    }
+                    else{
+                        weakTextView.text = "verified"
+                        weakTextView.setTextColor(resources.getColor(R.color.green))
+                    }
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+        })
+
+        monkeyReaction = binding.monkeyImage
+        monkeyReaction.setImageResource(R.drawable.close_monkey)
+        monkeyReaction.setOnClickListener{
+            if(isMonkeyOpen){
+
+                monkeyReaction.setImageResource(R.drawable.open_monkey)
+                isMonkeyOpen = false
+                passwordListener.inputType = InputType.TYPE_CLASS_TEXT
+                passwordListener.transformationMethod = HideReturnsTransformationMethod.getInstance()
+
+            }else{
+
+                monkeyReaction.setImageResource(R.drawable.close_monkey)
+                isMonkeyOpen = true
+
+                passwordListener.inputType = InputType.TYPE_TEXT_VARIATION_PASSWORD
+                passwordListener.transformationMethod = PasswordTransformationMethod.getInstance()
+            }
+            passwordListener.requestFocus()
+            (passwordListener as EditText).setSelection(passwordListener.text.length)
+        }
     }
 }
