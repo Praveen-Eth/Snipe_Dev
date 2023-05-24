cf1d484f8220014e9f612e327c722d66589573cf
praveen kumar <praveen0533y@gmail.com>
2023-05-24 22:33:37 +0530

    creating feed view UI

--- app/src/main/java/com/augx_universe/snipedev/adapters/FeedAdapter.kt
+++ app/src/main/java/com/augx_universe/snipedev/adapters/FeedAdapter.kt
@@ -1,10 +1,14 @@
 package com.augx_universe.snipedev.adapters
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.augx_universe.snipedev.R
 import com.augx_universe.snipedev.entities.FeedDataClass
+import com.augx_universe.snipedev.entities.PollItems
 import java.util.zip.Inflater
 
 class FeedAdapter(private val items: List<FeedDataClass>) : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
@@ -16,9 +20,45 @@ class FeedAdapter(private val items: List<FeedDataClass>) : RecyclerView.Adapter
 
     }
 
+    inner class PoleViewHolder(view: View) : RecyclerView.ViewHolder(view){
+        val titleView: TextView = view.findViewById(R.id.poll_title)
+         val poleOption1: CardView = view.findViewById(R.id.option1)
+        val poleOption2: CardView = view.findViewById(R.id.option2)
+        val poleOption3: CardView = view.findViewById(R.id.option3)
+        val poleOptionOther: CardView = view.findViewById(R.id.otherOption)
+
+        val option1TextView: TextView = view.findViewById(R.id.tv_option1)
+
+        val option2TextView: TextView = view.findViewById(R.id.tv_option2)
+        val option3TextView: TextView = view.findViewById(R.id.tv_option3)
+        val optionOtherTextView: TextView = view.findViewById(R.id.tv_option_other)
+        var numberOfPoleOptions: Int = 0
+
+        fun bind(pollItems: PollItems){
+            titleView.text = pollItems.question
+            numberOfPoleOptions = pollItems.options.size
+            option1TextView.text = " ${pollItems.options[0] } % ${pollItems.numberOfVotePerOption[0]}"
+            when (numberOfPoleOptions){
+                1 -> {
+
+                    poleOption2.visibility = View.GONE
+                    poleOption3.visibility = View.GONE
+
+                }
+                2 -> {
+                    poleOption3.visibility = View.GONE
+                }
+            }
+
+
+        }
+
+
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         return when(viewType) {
-            POLL_ITEM_VIEW_TYPE -> LayoutInflater.from(parent).inflate(R.layout.)
+            POLL_ITEM_VIEW_TYPE -> LayoutInflater.from(parent.context).inflate(R.layout.poll_item_layout,parent,false)
 
         }
     }
